30d5c38882ca509645aee1fc21cdce818f3cbd8f
MenendezON <menendezon@gmail.com>
2024-03-16 02:26:17 +0000

    Translate to french

--- resources/views/livewire/student/student-index.blade.php
+++ resources/views/livewire/student/student-index.blade.php
@@ -9,18 +9,18 @@
 
     <div class="flex">
         <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-            Dashboard hello
+            Liste des élèves
         </h2>
     </div>
 
     <div>
         <x-button wire:click="showCreatePostModal" class="mb-2">
-            {{ __('Add new student') }}
+            {{ __('Nouvel élève') }}
         </x-button>
         <!-- Create post modal -->
         <x-dialog-modal wire:model="createPostModal">
             <x-slot name="title">
-                {{ __('Create Student') }}
+                {{ __('Créer un élève') }}
             </x-slot>
 
             <form>
@@ -29,7 +29,7 @@
                         <div class="w-full md:w-1/3 mr-4 ml-4">
                             <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
-                                    First name
+                                    Prénom
                                 </span>
                                 <input wire:model="first_name" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" />
                                 @error('first_name')
@@ -40,7 +40,7 @@
                         <div class="w-full md:w-1/3 mr-4 ml-4">
                             <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
-                                    Last name
+                                    Nom
                                 </span>
                                 <input wire:model="last_name" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" />
                                 @error('last_name')
@@ -51,7 +51,7 @@
                         <div class="w-full md:w-1/3 mr-4 ml-4">
                             <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
-                                    Date of birth
+                                    Date de naissance
                                 </span>
                                 <input wire:model="date_of_birth" type="date" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" />
                                 @error('date_of_birth')
@@ -64,7 +64,7 @@
                         <div class="w-full md:w-1/3 mr-4 ml-4">
                             <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
-                                    Place of birth
+                                    Lieu de naissance
                                 </span>
                                 <input wire:model="place_of_birth" type="text" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" />
                                 @error('place_of_birth')
@@ -75,7 +75,7 @@
                         <div class="w-full md:w-1/4 mr-4 ml-4">
                             <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
-                                    Gender
+                                    Sexe
                                 </span>
                                 <select wire:model="gender" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-select focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
                                     <option>Select a gender...</option>
@@ -91,10 +91,10 @@
                         <div class="w-full md:w-1/3 mr-4 ml-4">
                             <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
-                                    Nationality
+                                    Nationalité
                                 </span>
                                 <select wire:model="nationality" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-select focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
-                                    <option>Select a country...</option>
+                                    <option>Sélectionner un pays...</option>
                                     <option value="Senegal">Senegal</option>
                                     <option value="Haiti">Haiti</option>
                                 </select>
@@ -109,10 +109,10 @@
                         <div class="w-full md:w-1/3 mr-4 ml-4">
                             <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
-                                    Blood group
+                                    Groupe sanguin
                                 </span>
                                 <select wire:model="blood_group" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-select focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
-                                    <option>Select a country...</option>
+                                    <option>Sélectionner un GS...</option>
                                     <option value="A+">A+</option>
                                     <option value="O+">O+</option>
                                     <option value="B+">B+</option>
@@ -141,7 +141,7 @@
                         <div class="w-full md:w-1/4 mr-4 ml-4">
                             <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
-                                    Phone number
+                                    Téléphone
                                 </span>
                                 <input type="tel" wire:model="phone" pattern="^\+221\d{9}$" title="Please enter a valid Senegalese phone number starting with +221 followed by 9 digits" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" />
                                 @error('phone')
@@ -154,7 +154,7 @@
                         <div class="w-full md:w-2/3 mr-4 ml-4">
                             <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
-                                    Full address
+                                    Adresse complète
                                 </span>
                                 <input wire:model="address" type="text" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" />
                                 @error('address')
@@ -165,7 +165,7 @@
                         <div class="w-full md:w-1/3 mr-4 ml-4">
                             <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
-                                    City
+                                    Ville
                                 </span>
                                 <input wire:model="city" type="text" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" />
                                 @error('city')
@@ -178,7 +178,7 @@
                         <div class="w-full md:w-1 mr-4 ml-4">
                             <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
-                                    Previous school
+                                    Ecole précédente
                                 </span>
                                 <input wire:model="previous_school" type="text" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" />
                                 @error('previous_school')
@@ -191,7 +191,7 @@
                         <div class="w-full md:w-1 mr-4 ml-4">
                             <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
-                                    Medical history
+                                    Vaccination
                                 </span>
                                 <textarea wire:model="medical_history" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"></textarea>
                                 @error('medical_history')
@@ -204,7 +204,7 @@
                         <div class="w-full md:w-1 mr-4 ml-4">
                             <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
-                                    Allergies
+                                    Allergie et diagnostique
                                 </span>
                                 <textarea wire:model="allergies" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"></textarea>
                                 @error('allergies')
@@ -215,7 +215,7 @@
                     </div>
                     <div class="flex px-2 my-2">
                         <div class="w-full md:w-1 mr-4 ml-4">
-                        <label class="block mt-4 text-sm">
+                            <label class="block mt-4 text-sm">
                                 <span class="text-gray-700 dark:text-gray-400">
                                     Réservé à l'administration
                                 </span>
@@ -236,7 +236,7 @@
                     <div class="px-2 my-2">
                         <div class="w-full md:w-2/3 mr-4 ml-4">
                             <x-button wire:click="create">
-                                {{ __('Create') }}
+                                {{ __('Créer') }}
                             </x-button>
                         </div>
                     </div>
@@ -251,13 +251,7 @@
                 <thead>
                     <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
                         <th class="px-4 py-3">
-                            <div class="flex item-center">#</div>
-                        </th>
-                        <th class="px-4 py-3">
-                            <div class="flex item-center">Prénom</div>
-                        </th>
-                        <th class="px-4 py-3">
-                            <div class="flex item-center">NOM</div>
+                            Nom
                         </th>
                         <th class="px-4 py-3">
                             <div class="flex item-center">Sexe</div>
@@ -266,7 +260,7 @@
                             <div class="flex item-center">Email</div>
                         </th>
                         <th class="px-4 py-3">
-                            <div class="flex item-center">Phone</div>
+                            <div class="flex item-center">Téléphone</div>
                         </th>
                         <th class="px-4 py-3">
                             <div class="flex item-center">Date de naissance</div>
@@ -274,41 +268,38 @@
                         <th class="px-4 py-3">
                             <div class="flex item-center">Lieu de naissance</div>
                         </th>
-                        <th class="px-4 py-3">Edité</th>
-                        <th class="px-4 py-3">Created by</th>
+                        <th class="px-4 py-3">
+                            <div class="flex item-center">Tuteur</div>
+                        </th>
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
                     @foreach($students as $student)
                     <tr class="text-gray-700 dark:text-gray-400" wire:key="{{$student->id}}">
+
                         <td class="px-4 py-3">
-                            <x-nav-link href="{{ route('student-show', ['id' => $student->id]) }}" wire:navigate>
-                                {{ $student->id }}
+                            <x-nav-link href="{{ route('student-show', ['id' => $student->id]) }}" wire:navigate class="flex items-center text-sm">
+                                <!-- Avatar with inset shadow -->
+                                <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
+                                    <img class="object-cover w-full h-full rounded-full" src="https://www.shareicon.net/data/512x512/2016/07/26/802043_man_512x512.png" alt="" loading="lazy" />
+                                    <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                                </div>
+                                <div>
+                                    <p>{{ ucwords($student->first_name) }}</p>
+                                    <p class="text-xs text-gray-600 dark:text-gray-400">
+                                        {{ strtoupper($student->last_name) }}
+                                    </p>
+                                </div>
                             </x-nav-link>
                         </td>
-                        <td class="px-4 py-3">{{ ucwords($student->first_name) }}</td>
-                        <td class="px-4 py-3 text-sm">{{ strtoupper($student->last_name) }}</td>
                         <td class="px-4 py-3 text-sm">{{ $student->gender? 'Male':'Female' }}</td>
                         <td class="px-4 py-3 text-sm"><a href="mailto:{{ $student->email }}">{{ $student->email?$student->email:'-' }}</a></td>
                         <td class="px-4 py-3 text-sm">{{ $student->phone?$student->phone:'-' }}</td>
                         <td class="px-4 py-3 text-sm">{{ \Carbon\Carbon::parse($student->date_of_birth)->format('d M Y') }}, {{ \Carbon\Carbon::parse($student->date_of_birth)->diffForHumans() }}</td>
                         <td class="px-4 py-3 text-sm">{{ $student->place_of_birth }}</td>
-                        <td class="px-4 py-3 text-sm">{{ $student->updated_at->diffForHumans() }}</td>
-                        <td class="px-4 py-3">
-                            <div class="flex items-center space-x-4 text-sm">
-                                <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
-                                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                                        <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z">
-                                        </path>
-                                    </svg>
-                                </button>
-                                <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
-                                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
-                                    </svg>
-                                </button>
-                            </div>
-                        </td>
+                        @foreach($student->tutors as $tutor)
+                        <td class="px-4 py-3 text-sm">{{ $tutor->relationship=='Tuteur'?$tutor->first_name:'' }} {{ $tutor->relationship=='Tuteur'?strtoupper($tutor->last_name):'' }}</td>
+                        @endforeach
                     </tr>
                     @endforeach
                 </tbody>
