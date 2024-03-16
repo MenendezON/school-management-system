1ef79481ad4c6f1914a7c3a1b170ca5a76a3cf80
MenendezON <menendezon@gmail.com>
2024-03-16 02:26:48 +0000

    Translate to french

--- resources/views/livewire/student/student-show.blade.php
+++ resources/views/livewire/student/student-show.blade.php
@@ -8,7 +8,7 @@
     @endif
 
     <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-            Dashboard
+            Dossier de l'élève
         </h2>
 
         <!-- Cards -->
