ce9267d3c650b1a0bbf6dd2c78eedd4503b81564
MenendezON <menendezon@gmail.com>
2024-03-22 07:37:06 +0000

    Update the survey model

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -9,7 +9,7 @@ class Survey extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title'];
+    protected $fillable = ['title', 'description'];
 
     public function questions()
     {
