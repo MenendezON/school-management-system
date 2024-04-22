59b66f8b36439bf7b22a0503b15cc83880a58453
MenendezON <menendezon@gmail.com>
2024-04-22 20:14:58 +0000

    Create the Medical component

---
+++ app/Livewire/Student/MedicalIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Student;
+
+use Livewire\Component;
+
+class MedicalIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.student.medical-index');
+    }
+}
