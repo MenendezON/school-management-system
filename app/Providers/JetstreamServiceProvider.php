dd21a8cd9e404a1144121b05964204b2cb307821
MenendezON <menendezon@gmail.com>
2024-04-30 09:52:57 +0000

    Update the role, add supervisor

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -57,5 +57,10 @@ class JetstreamServiceProvider extends ServiceProvider
             'create',
             'update',
         ])->description('Editor users have the ability to read, create, and update.');
+
+        Jetstream::role('supervisor', 'Supervisor', [
+            'read',
+            'update',
+        ])->description('Supervisor users have the ability to read, and update.');
     }
 }
