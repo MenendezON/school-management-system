474d1d75c4f704df87be535da7a983876ed4dda3
MenendezON <menendezon@gmail.com>
2024-05-09 23:58:35 +0000

    Implement the role into Medical' component

--- app/Livewire/Student/MedicalCreate.php
+++ app/Livewire/Student/MedicalCreate.php
@@ -4,6 +4,8 @@ namespace App\Livewire\Student;
 
 use App\Models\Medical;
 use App\Models\Student;
+use App\Models\Team;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\On;
 use Livewire\Component;
@@ -13,6 +15,7 @@ class MedicalCreate extends Component
     public $createMedicalModal = false;
     public $editMode = false;
     public ?Student $student;
+    public ?Medical $medical;
 
     public $field1;
 
@@ -30,12 +33,14 @@ class MedicalCreate extends Component
 
     public function create()
     {
-        //$this->validate();
+        
         if($this->editMode){
+            (!auth()->user()->canUpdate() || Auth::user()->currentTeam->id !== Team::find(1)->id) && abort(403, 'Unauthorized action.');
             $this->student->medical()->update($this->only(['field1']));
             request()->session()->flash("success", "Le dossier médical a été crée");
             $this->createMedicalModal = false;
         }else{
+            (!auth()->user()->canCreate() || Auth::user()->currentTeam->id !== Team::find(1)->id) && abort(403, 'Unauthorized action.');
             $this->student->medical()->create($this->only(['field1']));
             request()->session()->flash("success", "Le dossier médical a été crée");
             $this->createMedicalModal = false;
@@ -56,8 +61,16 @@ class MedicalCreate extends Component
         $this->showCreateMedicalModal();
     }
 
+    public function destroy()
+    {
+        (!auth()->user()->canDestroy() || Auth::user()->currentTeam->id !== Team::find(1)->id) && abort(403, 'Unauthorized action.');
+
+    }
+
     public function render()
     {
+        (!auth()->user()->canRead() || Auth::user()->currentTeam->id !== Team::find(1)->id) && abort(403, 'Unauthorized action.');
+
         $medical = Medical::where('student_id', $this->id)->first();
         return view('livewire.student.medical-create', [
             'medical'=> $medical
