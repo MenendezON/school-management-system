ed068f5a4113ac3a170e2f2ad8d565b675ee2d73
MenendezON <menendezon@gmail.com>
2024-05-16 16:02:44 +0000

    Switch the values position

--- app/Enums/SurveyValue.php
+++ app/Enums/SurveyValue.php
@@ -3,8 +3,8 @@ namespace App\Enums;
 
 enum SurveyValue: String
 {
-    case Fait_seul = "3";
-    case Fait_pas = "2";
-    case Fait_avec = "1";
     case Non_eval = "0";
+    case Fait_avec = "1";
+    case Fait_pas = "2";
+    case Fait_seul = "3";
 }
