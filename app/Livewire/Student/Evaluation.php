8a1a905cbc487d8e003c5df63e814653286ba112
MenendezON <menendezon@gmail.com>
2024-05-22 21:59:33 +0000

    Create the generateDoc function

--- app/Livewire/Student/Evaluation.php
+++ app/Livewire/Student/Evaluation.php
@@ -37,13 +37,71 @@ class Evaluation extends Component
             ->pluck('category');
 
         $this->data_analyse($request);
-
     }
 
     public function generateFileName($id)
     {
         $student = Student::findOrFail($id);
-        return str_replace(" ", "_", time()."-".ucwords($student->first_name).ucwords($student->last_name).".pdf");
+        return str_replace(" ", "_", time() . "-" . ucwords($student->first_name) . ucwords($student->last_name) . ".pdf");
+    }
+
+    public function generateDoc()
+    {
+
+        // Creating the new document...
+        $phpWord = new \PhpOffice\PhpWord\PhpWord();
+
+        /* Note: any element you append to a document must reside inside of a Section. */
+
+        // Adding an empty Section to the document...
+        $section = $phpWord->addSection();
+        // Adding Text element to the Section having font styled by default...
+        $section->addText(
+            '"Learn from yesterday, live for today, hope for tomorrow. '
+                . 'The important thing is not to stop questioning." '
+                . '(Albert Einstein)'
+        );
+
+        /*
+ * Note: it's possible to customize font style of the Text element you add in three ways:
+ * - inline;
+ * - using named font style (new font style object will be implicitly created);
+ * - using explicitly created font style object.
+ */
+
+        // Adding Text element with font customized inline...
+        $section->addText(
+            '"Great achievement is usually born of great sacrifice, '
+                . 'and is never the result of selfishness." '
+                . '(Napoleon Hill)',
+            array('name' => 'Tahoma', 'size' => 10)
+        );
+
+        // Adding Text element with font customized using named font style...
+        $fontStyleName = 'oneUserDefinedStyle';
+        $phpWord->addFontStyle(
+            $fontStyleName,
+            array('name' => 'Tahoma', 'size' => 10, 'color' => '1B2232', 'bold' => true)
+        );
+        $section->addText(
+            '"The greatest accomplishment is not in never falling, '
+                . 'but in rising again after you fall." '
+                . '(Vince Lombardi)',
+            $fontStyleName
+        );
+
+        // Adding Text element with font customized using explicitly created font style object...
+        $fontStyle = new \PhpOffice\PhpWord\Style\Font();
+        $fontStyle->setBold(true);
+        $fontStyle->setName('Tahoma');
+        $fontStyle->setSize(13);
+        $myTextElement = $section->addText('"Believe you can and you\'re halfway there." (Theodor Roosevelt)');
+        $myTextElement->setFontStyle($fontStyle);
+
+        // Saving the document as OOXML file...
+        $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+        $objWriter->save('helloWorld.docx');
+        return response()->download(public_path('helloWorld.docx'));
     }
 
     public function generatePdf()
@@ -55,27 +113,26 @@ class Evaluation extends Component
         ];
 
         $pdf = Pdf::loadView('livewire.pdf-evaluation', $data)
-        ->setPaper('a4', 'portrait');
+            ->setPaper('a4', 'portrait');
 
-        return response()->streamDownload(function() use($pdf){ 
+        return response()->streamDownload(function () use ($pdf) {
             echo $pdf->stream();
         }, $this->generateFileName($this->studentid));
-
     }
 
     public function data_analyse($request)
     {
-        foreach($this->categories as $category){
+        foreach ($this->categories as $category) {
             $this->options = DB::table('options')
-            ->join('questions', 'questions.id', '=', 'options.question_id')
-            ->join('students', 'options.student_id', '=', 'students.id')
-            ->where('student_id', $this->studentid)
-            ->where('quarter', $this->quarterid)
-            ->where('category', $category)
-            ->where('academic_year', $request->input('ay'))
-            ->get();
+                ->join('questions', 'questions.id', '=', 'options.question_id')
+                ->join('students', 'options.student_id', '=', 'students.id')
+                ->where('student_id', $this->studentid)
+                ->where('quarter', $this->quarterid)
+                ->where('category', $category)
+                ->where('academic_year', $request->input('ay'))
+                ->get();
 
-            foreach($this->options as $option){
+            foreach ($this->options as $option) {
                 $this->reports[$category][$option->question_id] = $this->extract_result($option);
             }
         }
@@ -87,10 +144,10 @@ class Evaluation extends Component
         switch ($opt->option_text) {
             case 3:
                 $result = $this->returnAnswer($opt, 'fait_seul');
-              break;
+                break;
             case 2:
                 $result = $this->returnAnswer($opt, 'fait_pas');
-              break;
+                break;
             case 1:
                 $result = $this->returnAnswer($opt, 'avec_aide');
                 break;
--- resources/views/livewire/student/evaluation.blade.php
+++ resources/views/livewire/student/evaluation.blade.php
@@ -10,24 +10,32 @@
     <div class="flex gap-2 justify-between">
 
         <h2 class="flex my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-        <x-nav-link href="{{ route('survey-eval-index', ['id' => $survey]) }}" class="bg-blue-500 pt-2 py-2 px-2 ml-2 rounded text-white" wire:navigate>
-            <svg width="30px" height="30px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <g id="icomoon-ignore">
-                </g>
-                <path d="M14.389 7.956v4.374l1.056 0.010c7.335 0.071 11.466 3.333 12.543 9.944-4.029-4.661-8.675-4.663-12.532-4.664h-1.067v4.337l-9.884-7.001 9.884-7zM15.456 5.893l-12.795 9.063 12.795 9.063v-5.332c5.121 0.002 9.869 0.26 13.884 7.42 0-4.547-0.751-14.706-13.884-14.833v-5.381z" fill="#ffffff">
+            <x-nav-link href="{{ route('survey-eval-index', ['id' => $survey]) }}" class="bg-blue-500 pt-2 py-2 px-2 ml-2 rounded text-white" wire:navigate>
+                <svg width="30px" height="30px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <g id="icomoon-ignore">
+                    </g>
+                    <path d="M14.389 7.956v4.374l1.056 0.010c7.335 0.071 11.466 3.333 12.543 9.944-4.029-4.661-8.675-4.663-12.532-4.664h-1.067v4.337l-9.884-7.001 9.884-7zM15.456 5.893l-12.795 9.063 12.795 9.063v-5.332c5.121 0.002 9.869 0.26 13.884 7.42 0-4.547-0.751-14.706-13.884-14.833v-5.381z" fill="#ffffff">
 
-                </path>
-            </svg>
+                    </path>
+                </svg>
             </x-nav-link>
-            
+
             <span>Rapport Psychoéducatif</span>
         </h2>
-        <button wire:click="generatePdf" title="Télécharger">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
-                <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1" />
-                <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1" />
-            </svg>
-        </button>
+        <div class="flex mx-6 gap-4">
+            <button wire:click="generateDoc" title="Télécharger">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
+                    <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1" />
+                    <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1" />
+                </svg>
+            </button>
+            <button wire:click="generatePdf" title="Télécharger">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
+                    <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1" />
+                    <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1" />
+                </svg>
+            </button>
+        </div>
     </div>
 
     <div class="w-full overflow-hidden rounded-lg shadow-xs">
