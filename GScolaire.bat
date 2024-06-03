12d0ab0b6e60de0ae8d732082c92808ea2704345
Menendez NELSON <menendezon@gmail.com>
2024-06-03 21:32:56 +0000

    Update GScolaire.bat

--- GScolaire.bat
+++ GScolaire.bat
@@ -7,19 +7,11 @@ REM Attente de quelques secondes pour que XAMPP se lance complètement
 timeout /t 10 /nobreak >nul
 
 REM Ouvrir le dossier gscolaire dans une nouvelle fenêtre de l'explorateur de fichiers
-start "" /d "C:\xampp\htdocs\gscolaire" 
+start "" /d "C:\xampp\htdocs"  
 REM Lancement de la commande "php artisan serve" pour le dossier précédent
-start cmd /k "cd C:\xampp\htdocs\gscolaire && php artisan serve"
-
-REM Ouvrir le dossier gscolaire dans une nouvelle fenêtre de l'explorateur de fichiers
-start "" /d "C:\xampp\htdocs\gscolaire" 
-REM Lancement de la commande "npm run dev" pour le dossier précédent
-start cmd /k "cd C:\xampp\htdocs\gscolaire && git checkout blockaccess && git pull origin blockaccess && npm run dev"
+start cmd /k "cd C:\xampp\htdocs && rmdir /S /Q .\gscolaire"
 
 REM Attente de quelques secondes pour que les serveurs se lancent
 timeout /t 10 /nobreak >nul
-
-REM Lancement du navigateur avec le lien http://127.0.0.1:8000/
-start http://127.0.0.1:8000/
 
 exit
